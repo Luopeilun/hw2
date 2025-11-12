bc6b02cdd6579eafce2b6f7d7d5ab5a492eae663
Luopeilun <161785265+Luopeilun@users.noreply.github.com>
2025-11-12 22:05:19 +0800

    Update Sample2_2.java

--- hw2/優化ch2/Sample2_2.java
+++ hw2/優化ch2/Sample2_2.java
@@ -2,21 +2,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Sample2_2_Error {
-    public static void main(String[] args) throws IOException { // 簡化寫法，實際應使用 try-catch
-        System.out.println("請輸入整數");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+public class Sample2_2_Correct {
+    public static void main(String[] args) {
+        // ... (使用 try-with-resources 讀取輸入)
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("請輸入整數：");
+            int num = Integer.parseInt(br.readLine());
 
-        // 【錯誤示範】
-        // 因為沒有使用大括號 {}，所以只有下一行 "System.out.println("輸入的是1");" 屬於 if 的範圍
-        if (num == 1)
-            System.out.println("輸入的是1"); // 此行只有在 num 為 1 時執行
+            // 【正確寫法】
+            // 使用大括號 {} 將所有希望在條件成立時執行的程式碼包起來
+            if (num == 1) {
+                System.out.println("輸入的是1");
+                System.out.println("選擇的是1");
+            }
+            
+            System.out.println("結束處理。");
 
-        // 【邏輯陷阱】
-        // 此行不屬於 if 的條件範圍，因此不論 num 是多少，它都會被執行
-        System.out.println("選擇的是1"); 
-        
-        System.out.println("結束處理");
+        } catch (Exception e) {
+            System.out.println("處理輸入時發生錯誤。");
+        }
     }
 }
